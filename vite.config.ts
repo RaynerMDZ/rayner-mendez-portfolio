dea8ab0707ea71e8711589a4181d4b298c5edded
Rayner Mendez <34711027+RaynerMDZ@users.noreply.github.com>
2025-09-02 00:31:18 -0400

    add config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {componentTagger} from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => ({
-    base: "rayner-mendez-portfolio",
+    base: mode === 'production' ? 'rayner-mendez-portfolio' : '/',
     server: {
         host: "::",
         port: 8080,
